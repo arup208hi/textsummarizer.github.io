905f58b4e9358c86a48de8aad0f4214fd69f90b9
Arup Das <71006072+arup208hi@users.noreply.github.com>
2022-06-25 01:13:55 +0530

    summarization website

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from main1 import nltk_summarizer
+from main2 import text_summarizer
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['GET', 'POST'])
+def analyze():
+    if request.method == 'POST':
+        text = request.form['text']
+        nltk_summary = nltk_summarizer(text)
+        spacy_summary = text_summarizer(text)
+    return render_template('index.html', text=text, nltk_summary=nltk_summary, spacy_summary=spacy_summary)
+    
+    
+if __name__ == "__main__":
+    app.run()
